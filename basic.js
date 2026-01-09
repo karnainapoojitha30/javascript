da28ea5e4699f7a8d8865fcbce14357bbef3a13e
karnainapoojitha30 <poojithalakshmi3@gmail.com>
2026-01-09 11:27:38 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,6 @@
+let name="poojitha";
+const age=21;
+console.log("Name:",name,"datatype:",typeof(name));
+console.log("Age:",age,"datatype:",typeof(age));
+let studentis=true;
+console.log("studentis:",studentis,"datatype:",typeof(studentis));
